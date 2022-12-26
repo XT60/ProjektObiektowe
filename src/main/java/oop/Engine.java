7cc98c38e5c475686b99503b166a0ea47a639742
Endrew <azab@student.agh.edu.pl>
2022-12-26 22:46:54 +0100

    added ideas how to implement Engine to Engine.java

--- src/main/java/oop/Engine.java
+++ src/main/java/oop/Engine.java
@@ -2,5 +2,28 @@ package oop;
 
 public class Engine {
 
+// tworzenie mapy:
+    // IMap map = new GlobeMap(MapConstants)    lub     new PortalMap(MapConstants)
+// tworzenie roślin
+    // IPlant plants = new ToxicCorpse(MapConstants, AbstractMap.getDeadAnimalHolder)   lub  PlantsEquator(MapConstants)
+
+// poruszanie się zwierząt:
+    //// Vector2d newPosition = animal.turn();
+    //// if map.canMoveTo(newPosition)
+    ////      animal.move(map.changePosition(newPosition))
+    //// else
+    ////      animal.turn
+// dodawanie roślin do mapy:
+    //
+
+
+// zjadanie roślin:
+    // animals = map.getAnimals()
+    // for position in animals.keys():
+    //      if(plants.isPlantAtPositoin(position)){
+    //          animals.get(position).get(0).feed()
+    //          plants.removePlant
+    //      }
+
 
 }
